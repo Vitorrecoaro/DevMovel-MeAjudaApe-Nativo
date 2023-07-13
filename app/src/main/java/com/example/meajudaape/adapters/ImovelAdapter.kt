1150d025add7df13203d35b5ddc3892b906df4e2
Thiago Albino <thiagoalbino2000@hotmail.com>
2023-07-13 00:19:27 -0300

    Tentando trocar de tela

--- app/src/main/java/com/example/meajudaape/adapters/ImovelAdapter.kt
+++ app/src/main/java/com/example/meajudaape/adapters/ImovelAdapter.kt
@@ -1,18 +1,20 @@
 package com.example.meajudaape.adapters
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meajudaape.classes.Imovel
 import com.example.meajudaape.databinding.CardImovelBinding
-import com.example.meajudaape.holders.ImovelHolder
+import com.example.meajudaape.Descricao
 
-class ImovelAdapter (private val imoveis: List<Imovel>) : RecyclerView.Adapter<ImovelHolder>() {
+class ImovelAdapter(private val imoveis: List<Imovel>) : RecyclerView.Adapter<ImovelAdapter.ImovelHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImovelHolder {
-        val from = LayoutInflater.from(parent.context)
-        val binding = CardImovelBinding.inflate(from, parent, false)
-
-        return ImovelHolder(binding);
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = CardImovelBinding.inflate(inflater, parent, false)
+        return ImovelHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -20,6 +22,21 @@ class ImovelAdapter (private val imoveis: List<Imovel>) : RecyclerView.Adapter<I
     }
 
     override fun onBindViewHolder(holder: ImovelHolder, position: Int) {
-        holder.bindBook(imoveis[position])
+        val imovel = imoveis[position]
+        holder.bindImovel(imovel)
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, Descricao::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    inner class ImovelHolder(private val binding: CardImovelBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bindImovel(imovel: Imovel) {
+            binding.imovelAluguel.text = "R$ ${imovel.aluguel.toString()}"
+            binding.imovelCondominio.text = "R$ ${imovel.condominio.toString()}"
+            binding.imovelDescricao.text = imovel.descricao
+            binding.imovelTitulo.text = imovel.nome
+        }
     }
 }
--- app/src/main/java/com/example/meajudaape/holders/ImovelHolder.kt
+++ app/src/main/java/com/example/meajudaape/holders/ImovelHolder.kt
@@ -1,11 +1,14 @@
 package com.example.meajudaape.holders
 
+import android.content.Context
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meajudaape.classes.Imovel
 import com.example.meajudaape.databinding.CardImovelBinding
 
-class ImovelHolder(private val cardImovelBinding: CardImovelBinding) : RecyclerView.ViewHolder(cardImovelBinding.root) {
+class ImovelHolder(private val cardImovelBinding: CardImovelBinding,
+    val context: Context)
+    : RecyclerView.ViewHolder(cardImovelBinding.root) {
     fun bindBook(imovel: Imovel){
         cardImovelBinding.imovelAluguel.text = "R$ ${imovel.aluguel.toString()}"
         cardImovelBinding.imovelCondominio.text = "R$ ${imovel.condominio.toString()}"
